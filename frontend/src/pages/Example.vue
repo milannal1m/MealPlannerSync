9d7d71dbd7c65f504d42a532782491fbaf8bfb93
Milan <milan.zweit@gmail.com>
2025-04-01 17:29:02 +0200

    New fetch URLs

--- frontend/src/pages/Example.vue
+++ frontend/src/pages/Example.vue
@@ -1,7 +1,7 @@
 
 <-- GET -->
 
-fetch("http://localhost/meal/test/meals")
+fetch("http://" + window.location.hostname + "/meal/test/meals")
   .then(response => response.json())
   .then(data => console.log(data))
   .catch(error => console.error("Error:", error));
@@ -9,7 +9,7 @@ fetch("http://localhost/meal/test/meals")
 
 <-- POST -->
 
-const url = new URL("http://localhost/meal/test/meals");
+const url = new URL("http://" + window.location.hostname + "/meal/test/meals");
 url.searchParams.append("name", "Pizza");
 url.searchParams.append("date", "2025-03-30T12:00:00");
 
@@ -24,7 +24,7 @@ fetch(url, {
 
 <-- DELETE -->
 
-fetch("http://localhost/meal/test/meals/6", {
+fetch("http://" + window.location.hostname + "/meal/test/meals/6", {
   method: "DELETE"
 })
   .then(response => {
@@ -38,14 +38,14 @@ fetch("http://localhost/meal/test/meals/6", {
 
 <-- Web Sockets -->
 
-const meal_socket = new WebSocket("http://localhost/meal/ws");
+const meal_socket = new WebSocket("http://" + window.location.hostname + "/meal/ws");
 
 meal_socket.onmessage = function(event) {
     console.log("Nachricht vom Server:", event.data);
     // Hier refreshen wir die Liste der Meals
 };
 
-const user_socket = new WebSocket("http://localhost/user/ws");
+const user_socket = new WebSocket("http://" + window.location.hostname + "/user/ws");
 
 user_socket.onmessage = function(event) {
     console.log("Nachricht vom Server:", event.data);
