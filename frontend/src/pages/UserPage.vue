4f97a139f563a3084093faf448fc3b59c7069659
Max Kuebler <maximilian@kuebler.de>
2025-04-02 11:38:48 +0200

    Added fix for reload error

--- frontend/src/pages/UserPage.vue
+++ frontend/src/pages/UserPage.vue
@@ -2,6 +2,7 @@
 
   import BackButton from '@/components/BackButton.vue'; 
   import AddButtonUser from '@/components/AddButtonUser.vue';
+  
 
   export default {
 
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 import HomePage from '../pages/HomePage.vue';
 import UserPage from '../pages/UserPage.vue';
@@ -18,7 +18,7 @@ const routes = [
 
 export default createRouter ({
 
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes
 
 });
