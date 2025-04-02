483da49ea064b9bf977e66a2f26de6a1244800aa
Max Kuebler <maximilian@kuebler.de>
2025-04-02 15:06:48 +0200

    Added WebSocket to MealList

--- frontend/src/components/MealList.vue
+++ frontend/src/components/MealList.vue
@@ -40,6 +40,18 @@
 
         location.reload();
       };
+
+      const user_socket = new WebSocket("http://" + window.location.hostname + "/user/ws");
+
+          user_socket.onopen = function() {
+            console.log("WebSocket ist verbunden.");
+          };
+
+          user_socket.onmessage = function(event) {
+            console.log("Nachricht vom Server:", event.data);
+            
+            location.reload();
+          };
     },
 
     methods: {
